58a671c97224094141e5c236d2ad47726d44ee34
fpellet <git@florentpellet.com>
2019-10-15 11:23:25 +0200

    :construction: add some better stuff than benj and max and rocketman

--- Java/src/main/java/BookingReference.java
+++ Java/src/main/java/BookingReference.java
@@ -1,7 +1,27 @@
+import java.util.Objects;
+
 public class BookingReference {
     public final String reference;
 
     public BookingReference(String reference) {
         this.reference = reference;
+    }
+
+    @Override
+    public String toString() {
+        return reference;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookingReference that = (BookingReference) o;
+        return Objects.equals(reference, that.reference);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reference);
     }
 }
