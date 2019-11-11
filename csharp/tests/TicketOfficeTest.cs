1a4966f93fb754dc355bd2f3e16685d98c7b78af
fpellet <git@florentpellet.com>
2019-11-11 18:38:01 +0100

    create project files

---
+++ csharp/tests/TicketOfficeTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace KataTrainReservation
+{
+    public class TicketOfficeTest {
+    
+        [Fact]
+        public void ReserveSeats() {
+            // TODO: write this code
+
+        }
+    }
+}
